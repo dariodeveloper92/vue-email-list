f6e14d78b6a5b49c28b63046e935649aeb8a02d6
Dario <dario.defrancisci92@gmail.com>
2021-09-16 13:13:22 +0200

    creato scaffolding

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/*Esercizio:
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.*/
+
+
+/*Bonus:
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.*/
